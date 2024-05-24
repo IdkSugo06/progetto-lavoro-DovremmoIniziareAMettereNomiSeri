686d41e858eb8a7d27fa50c3eac66fac8ac9bf3a
IdkSugo06 <simo.ugo@fastwebnet.it>
2024-05-24 08:52:32 +0200

    as

--- GestionePagine/Pagine/Pagine.py
+++ GestionePagine/Pagine/Pagine.py
@@ -1,5 +1,5 @@
-from GestionePagine.Pagine.PaginaDispositivi import *
 from GestionePagine.Pagine.PaginaDashboard import *
+from GestionePagine.Pagine.PaginaDispositivi import *
 from GestionePagine.Pagine.PaginaAggiuntaDispositivo import *
 from GestionePagine.Pagine.PaginaModificaDispositivo import *
 from GestionePagine.Pagine.PaginaImpostazioni import *
